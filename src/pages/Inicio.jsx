ca4a3e7cfcf917a7dbcf52c67c67cbf3b287e8e9
Yeison Cuenca <ys.cuenca@outlook.com>
2025-09-10 17:10:19 -0500

    Finalizacion version1

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -15,25 +15,34 @@ const Inicio = () => {
       </section>
       <hr className="separador"/>
       <section className="section">
-        <h2>¿Qué es un Organismo de Inspección?</h2>
+        <div className="section-content parrafo">
+          <h2>¿Qué es un Organismo de Inspección?</h2><br></br><br></br>
+          <p>
+            Cuando hablamos de organismos de inspección, nos referimos a las organizaciones que realizan inspecciones. 
+            Tanto aquellas que se dedican a la ejecución de estas actividades, 
+            como aquellas que hacen parte de otras donde se requiera de este tipo de servicios. Esto incluye:<br></br> </p>
+          <p>&bull; El examen de materiales, productos, procesos, instalaciones, plantas, procedimientos de trabajo o servicios; <br></br></p>
+          <p>&bull; La determinación de su conformidad con los requisitos; <br></br></p>
+          <p>&bull; La emisión del informe de los resultados de estas actividades; <br></br></p>
+          <p>&bull; Y su comunicación a los clientes y autoridades.         <br></br></p>
+        </div>
         <div className="section-content">
           <img src={"/images/inspeccio.jpg"} alt="Inspección" />
-          <p>
-            Cuando hablamos de organismos de inspección, nos referimos a las organizaciones que realizan inspecciones. Tanto aquellas que se dedican a la ejecución de estas actividades, como aquellas que hacen parte de otras donde se requiera de este tipo de servicios. Esto incluye:
-            El examen de materiales, productos, procesos, instalaciones, plantas, procedimientos de trabajo o servicios;
-            La determinación de su conformidad con los requisitos;
-            La emisión del informe de los resultados de estas actividades; y
-            Su comunicación a los clientes y autoridades.          </p>
         </div>
       </section>
       <section className="section alt">
-        <h2>Gestión del riesgo según ISO 17020</h2>
         <div className="section-content">
-          <img src={"/images/tablacheck.png"} alt="Riesgo ISO 17020" />
-          <p>
-            En el caso de los organismos de inspección, la identificación incluye determinar qué actividades, que relaciones, o que condiciones del personal pueden generar situaciones, condiciones o acciones susceptibles de producir de los eventos que pueden afectar su imparcialidad.
-            Por otra parte, incluye determinar cómo la propiedad, la gobernabilidad, la dirección, los recursos compartidos, las finanzas, los contratos, el marketing, el pago de una comisión por ventas u otros incentivos pueden generar sesgos o conflictos de interés.
-            No obstante lo anterior, la norma no especifica cómo hacerlo. Por esta razón, para entender qué son los riesgos y cómo se gestionan a nivel general, puedes ver el siguiente vídeo donde lo explicamos con mayor detalle.
+          <img src={"/images/GestionDelRiesgo.png"} alt="Riesgo ISO 17020" />
+        </div>
+        <div className="section-content parrafo">
+          <p className="parrafo">
+            En el caso de los organismos de inspección, la identificación incluye determinar qué actividades, que relaciones, 
+            o que condiciones del personal pueden generar situaciones, condiciones o acciones susceptibles de producir de los 
+            eventos que pueden afectar su imparcialidad.
+            Por otra parte, incluye determinar cómo la propiedad, la gobernabilidad, la dirección, los recursos compartidos, las finanzas, 
+            los contratos, el marketing, el pago de una comisión por ventas u otros incentivos pueden generar sesgos o conflictos de interés.
+            No obstante lo anterior, la norma no especifica cómo hacerlo. Por esta razón, para entender qué son los riesgos y cómo se gestionan 
+            a nivel general, puedes ver el siguiente vídeo donde lo explicamos con mayor detalle.
           </p>
         </div>
       </section>
